e4322b852207a33c63587fe1d5264c67403bf950
Unknown <mcmastasadventures@gmail.com>
2017-10-08 12:13:40 +0200

    Readd all io-grid files in old directory

---
+++ old/utility.nim
@@ -0,0 +1,17 @@
+import jsffi
+
+proc consoleLog*() {.varargs, importc:"console.log".}
+
+proc jsSocketEmit*(id: cstring, data: JsObject,
+  callback: proc(err: JsObject, data: JsObject)) {.importc.}
+
+proc jsAddSprite(x, y: int, textureName: cstring) : JsObject {.importc.}
+proc createSprite*(textureName: cstring): JsObject =
+  result = jsAddSprite(0, 0, textureName)
+  result.anchor.setTo(0.5)
+
+# var phaserGame* {.importc:"game".} : JsObject
+# var phaserGameAdd* {.importc:"game.add".} : JsObject
+proc phaserGameAddTileSprite*(x, y, width, height: int, texture: cstring) : JsObject {.importc:"game.add.tileSprite".}
+
+proc phaserLoadImage*(name, path: cstring) {.importc:"game.load.image".}
